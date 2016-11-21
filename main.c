86e0c1a9ffd9dab715125e53e566f84229bc83b7
Leo <leononame@lho.io>
2016-11-21 16:23:50 +0100

    Generate key and IV

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
+#include <openssl/rand.h>
+
 
 
 int main(int argc, char **argv) {
-    printf("Hello, world!");
+    /* Key and IV */
+    unsigned char key[32], iv[16];
+
+    if (!RAND_bytes(key, sizeof key)) {
+        /* OpenSSL reports a failure, act accordingly */
+    }
+    if (!RAND_bytes(iv, sizeof iv)) {
+        /* OpenSSL reports a failure, act accordingly */
+    }
+    for(int i=0; i<sizeof key; i++) {
+        printf("0x%2x ", key[i]);
+    }
+    printf("\n");
+    for(int i=0; i<sizeof iv; i++) {
+        printf("0x%2x ", iv[i]);
+    }
+    printf("\n");
+
     return 0;
 }
