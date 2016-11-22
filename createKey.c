c5322e5e3179e52b91ea184868174d4559f89dbb
Leo <leononame@lho.io>
2016-11-22 10:47:01 +0100

    Some better code handling

--- createKey.c
+++ createKey.c
@@ -7,6 +7,16 @@
 /// Creates a new TPM key.
 /// \return integer
 int TPM_CreateKey(void);
+/// \brief Checks if a key exists.
+///
+/// Checks if both the TPM and the AES key exist. If one of them exists, it will prompt the user to
+/// run the program with the switch -f to force key creation and return 1. If the switch -f is present,
+/// it will ignore whether the key exists or not.
+/// \return integer
+int CheckKey(void);
+/// Closes open handles and exits with failure code.
+void ExitFailure(void);
+
 int override = 0; ///< Tells whether to override existing key or not.
 
 int main(int argc, char **argv) {
@@ -21,16 +31,46 @@ int main(int argc, char **argv) {
         }
     }
     // Initialize TPM context
-    if(TPM_InitContext() != 0)
-        exit(EXIT_FAILURE);
+    if(TPM_InitContext())
+        ExitFailure();
+    // Check if key exists
+    if (CheckKey())
+        ExitFailure();
     // Create TPM key
-    if(TPM_CreateKey() != 0)
-        exit(EXIT_FAILURE);
+    if(TPM_CreateKey())
+        ExitFailure();
+    
     // Close all open TPM handles
     TPM_CloseContext();
     return 0;
 }
 
+void ExitFailure(void) {
+    TPM_CloseContext();
+    exit(EXIT_FAILURE);
+}
+int CheckKey(void) {
+    // Check if key exists by loading it from UUID and checking if the keyfile is present.
+    TSS_HKEY hKey;
+    TSS_RESULT result;
+    result = Tspi_Context_LoadKeyByUUID(hContext, TSS_PS_TYPE_SYSTEM, KEY_UUID, &hKey);
+    Tspi_Context_CloseObject(hContext, hKey);
+
+    // If key exists
+    int exists = fileExists(KEYPATH) | (result == TSS_SUCCESS);
+
+    if (exists && !override) {
+        printf("Key already exists. If you wish to override the existing key, please run this program with switch -f. This will create a new AES-256 key and a new TPM key.\n");
+        return 1;
+    } else if (exists) {
+        int c;
+        printf("Overriding old key. All data encrypted with it will be lost. Proceed? (y/n) ");
+        c = getchar();
+        if (c != 'y' && c != 'Y')
+            return 1;
+    }
+    return 0;
+}
 int TPM_CreateKey(){
     // TPM variables
     TSS_HKEY hBindKey;
@@ -41,20 +81,6 @@ int TPM_CreateKey(){
                          TSS_KEY_AUTHORIZATION |
                          TSS_KEY_NOT_MIGRATABLE |
                          TSS_KEY_STRUCT_KEY12;
-    
-    // If key exists
-    int exists = fileExists(KEYPATH);
-
-    if (exists && !override) {
-        printf("Key already exists. If you wish to override the existing key, please run this program with switch -f.\n");
-        return 1;
-    } else if (exists) {
-        int c;
-        printf("Overriding old key. All data encrypted with it will be lost. Proceed? (y/n) ");
-        c = getchar();
-        if (c != 'y' && c != 'Y')
-            return 1;
-    }
 
     // Unregister old bind key if it exists
     Tspi_Context_UnregisterKey(hContext, TSS_PS_TYPE_SYSTEM, BindKey_UUID, &hBindKey);
